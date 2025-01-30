6ea0ee8a692d331e1238fed82ea5532315ec7208
Jerry Rondon <jerryrondon@gmail.com>
2025-01-29 23:30:39 -0400

    Soluciona el problema con env_file del yaml
    https://github.com/docker/compose/issues/9443

--- flask/app.py
+++ flask/app.py
@@ -8,3 +8,11 @@ def about():
     version = os.environ.get('APP_VERSION')
 
     return {'app_version': version}, 200
+
+
+@app.route("/secrets", methods=['GET'])
+def secrets():
+    creds = dict()
+    creds['db_password'] = os.environ.get('DB_PASSWORD')
+
+    return creds, 200
